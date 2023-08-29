b611a6fbac7586a9d3fa04b0a9ba761e631a28c6
Russell Jones <justyrones@gmail.com>
2023-08-29 17:26:51 -0600

    added the heroku front end to the cors call

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ const morgan = require("morgan");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://friender-frontend-6106516b52e7.herokuapp.com'
+}));
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(authenticateJWT);
