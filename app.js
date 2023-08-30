b26672f5a426b048324dd51da1cdba48a2c29c60
Russell Jones <justyrones@gmail.com>
2023-08-29 18:11:58 -0600

    commented out entire cors policy to see if that is the problem or not

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 
 const { NotFoundError } = require("./expressError");
 
@@ -15,7 +15,7 @@ const morgan = require("morgan");
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(authenticateJWT);
